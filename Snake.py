542033a19d32fd821147ca6dbb6858e0c44bf5a8
keyles-Py <162361852+keyles-Py@users.noreply.github.com>
2024-03-05 13:35:35 -0500

    Snake.py

--- Snake.py
+++ Snake.py
@@ -1,42 +1,12 @@
-from random import randrange
-from secrets import choice, randbelow
 import pygame
 from pygame.locals import *
 from pygame import mixer
 import sys
-import os
-import time
+from clases import nave, Estrellas, Meteoro
 
 #Ancho y alto de la ventana del juego
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 625
-
-class nave(pygame.sprite.Sprite): # Clase que va a tener las caracterissticas de las naves
-
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("Snake/medios/rocket_up.png").convert_alpha()
-        self.rect = self.image.get_rect()
-        self.rect.top = 550
-        self.rect.left = 500
-
-class Estrella(pygame.sprite.Sprite):
-
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("Snake/medios/resizeimagestar.png").convert_alpha()
-        self.rect = self.image.get_rect()
-        self.rect.top = choice([i*5 for i in range(6,100)])
-        self.rect.left = choice([i*5 for i in range(1,190)])
-
-class Meteoro(pygame.sprite.Sprite):
-
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("Snake/medios/meteoro2.png").convert_alpha()
-        self.rect = self.image.get_rect()
-        self.rect.top = choice([i*5 for i in range(6,100)])
-        self.rect.left = choice([i*5 for i in range(1,190)])
 
 def play_background_music():
     # Comprueba si la canción ya está reproduciéndose
